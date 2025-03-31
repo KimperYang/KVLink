4ce5a85d97df76456dd0a44d65d4ecb373edba78
kimperyang <jingbo@ucsb.edu>
2025-03-31 04:47:25 +0000

    update

--- src/training/torchtune_model_checkpointer.py
+++ src/training/torchtune_model_checkpointer.py
@@ -8,7 +8,11 @@ import torch
 from torchtune.models import convert_weights
 from torchtune.training.checkpointing._utils import (
     safe_torch_load,
+    ModelType,
 )
+from torchtune import training
+from torchtune.utils import get_logger, get_world_size_and_rank, log_rank_zero
+import torchtune
 
 MODEL_CONFIG_DICT = {
     "meta-llama/Llama-3.2-1B-Instruct": {
@@ -20,10 +24,7 @@ MODEL_CONFIG_DICT = {
     }
 }
 
-def load_checkpoint(
-    ckpt_path: str,
-    model_name: str,
-) -> Dict[str, Any]:
+def load_checkpoint(self) -> Dict[str, Any]:
     """
     Load HF checkpoint from file.
 
@@ -34,15 +35,14 @@ def load_checkpoint(
     Before returning, the model state dict is converted to a torchtune-compatible format using
     the appropriate convert_weights function (depending on ``self._model_type``).
 
-    Args:
-        ckpt_path: the path that store the model checkpoint downloaded from huggingface `original`
-        model_name: the HF model name, such as `meta-llama/Llama-3.2-1B-Instruct`
     Returns:
         state_dict (Dict[str, Any]): torchtune checkpoint state dict
 
     Raises:
         ValueError: If the values in the input state_dict are not Tensors
     """
+
+    self._weight_map = {}
 
     # merged state_dict contains keys and weights from all the checkpoint files
     merged_state_dict: Dict[str, torch.Tensor] = {}
@@ -53,23 +53,177 @@ def load_checkpoint(
     converted_state_dict: Dict[str, Dict[str, torch.Tensor]] = {}
 
     # _checkpoint_paths are already sorted so simply enumerate to generate the right id
-    state_dict = safe_torch_load(ckpt_path)
-    merged_state_dict.update(state_dict)
+    for cpt_idx, cpt_path in enumerate(self._checkpoint_paths):
+        state_dict = safe_torch_load(cpt_path)
+        for key, value in state_dict.items():
+            # Ensure that the state dict is a flat dict of keys and tensors. Breaking this assumption
+            # will break recipe code
+            if not isinstance(value, torch.Tensor):
+                raise ValueError(
+                    f"Expected all values in the state dict to be torch.Tensor. "
+                    f"Found {type(value)} instead."
+                )
+            # idx is written in the 4 digit format (eg: 0001, 0002, etc.)
+            self._weight_map[key] = f"{cpt_idx + 1:04}"
+        merged_state_dict.update(state_dict)
 
-    # delete the state_dict to free up memory; TODO check if this del is needed
-    del state_dict
-    gc.collect()
+        # delete the state_dict to free up memory; TODO check if this del is needed
+        del state_dict
+        gc.collect()
+    # if self._model_type in (ModelType.PHI3_MINI, ModelType.PHI4):
+    #     log_rank_zero(
+    #         logger=logger,
+    #         msg="Converting Phi weights from HF format."
+    #         "Note that conversion of adapter weights into PEFT format is not supported.",
+    #     )
+    #     from torchtune.models.phi3._convert_weights import phi3_hf_to_tune
 
-    cfg_dict = MODEL_CONFIG_DICT[model_name]
-    converted_state_dict = convert_weights.hf_to_tune(
-        merged_state_dict,
-        num_heads=cfg_dict["num_attention_heads"],
-        num_kv_heads=cfg_dict["num_key_value_heads"],
-        dim=cfg_dict["hidden_size"],
-        head_dim=cfg_dict.get("head_dim", None),
-    )
+    #     num_heads = self._config["num_attention_heads"]
+    #     num_kv_heads = self._config["num_key_value_heads"]
+    #     dim = self._config["hidden_size"]
+
+    #     # Should only pass num_heads, num_kv_heads, dim for GQA
+    #     if num_heads == num_kv_heads:
+    #         num_heads, num_kv_heads, dim = None, None, None
+
+    #     converted_state_dict[training.MODEL_KEY] = phi3_hf_to_tune(
+    #         merged_state_dict,
+    #         num_heads=num_heads,
+    #         num_kv_heads=num_kv_heads,
+    #         dim=dim,
+    #     )
+    if self._model_type == ModelType.REWARD:
+        from torchtune.rlhf.utils import reward_hf_to_tune
+
+        converted_state_dict[training.MODEL_KEY] = reward_hf_to_tune(
+            merged_state_dict,
+            num_heads=self._config["num_attention_heads"],
+            num_kv_heads=self._config["num_key_value_heads"],
+            dim=self._config["hidden_size"],
+        )
+    elif self._model_type == ModelType.QWEN2:
+        from torchtune.models.qwen2._convert_weights import qwen2_hf_to_tune
+
+        converted_state_dict[training.MODEL_KEY] = qwen2_hf_to_tune(
+            merged_state_dict,
+            num_heads=self._config["num_attention_heads"],
+            num_kv_heads=self._config["num_key_value_heads"],
+            dim=self._config["hidden_size"],
+            tie_word_embeddings=self._config["tie_word_embeddings"],
+        )
+    elif self._model_type == ModelType.LLAMA3_VISION:
+        from torchtune.models.llama3_2_vision._convert_weights import (
+            llama3_vision_hf_to_tune,
+        )
+
+        text_config = self._config.get("text_config", {})
+        vision_config = self._config.get("vision_config", {})
+        converted_state_dict[training.MODEL_KEY] = llama3_vision_hf_to_tune(
+            merged_state_dict,
+            num_heads=text_config["num_attention_heads"],
+            num_kv_heads=text_config["num_key_value_heads"],
+            dim=text_config["hidden_size"],
+            head_dim=text_config.get("head_dim", None),
+            vocab_size=text_config["vocab_size"],
+            cross_attention_layers=text_config.get("cross_attention_layers", None),
+            encoder_dim=vision_config["hidden_size"],
+            tile_size=vision_config["image_size"],
+            num_tiles=vision_config["max_num_tiles"],
+            supported_aspect_ratios=vision_config.get(
+                "supported_aspect_ratios", None
+            ),
+        )
+    elif self._model_type == ModelType.CLIP_TEXT:
+        from torchtune.models.clip._convert_weights import clip_text_hf_to_tune
+
+        converted_state_dict[training.MODEL_KEY] = clip_text_hf_to_tune(
+            merged_state_dict,
+        )
+    elif self._model_type == ModelType.GEMMA2:
+        from torchtune.models.gemma2._convert_weights import gemma2_hf_to_tune
+
+        converted_state_dict[training.MODEL_KEY] = gemma2_hf_to_tune(
+            merged_state_dict,
+            num_heads=self._config["num_attention_heads"],
+            num_kv_heads=self._config["num_key_value_heads"],
+            dim=self._config["hidden_size"],
+            head_dim=self._config.get("head_dim", None),
+        )
+    elif self._model_type == ModelType.T5_ENCODER:
+        from torchtune.models.t5._convert_weights import t5_encoder_hf_to_tune
+
+        converted_state_dict[training.MODEL_KEY] = t5_encoder_hf_to_tune(
+            merged_state_dict,
+        )
+    else:
+        converted_state_dict[training.MODEL_KEY] = convert_weights.hf_to_tune(
+            merged_state_dict,
+            num_heads=self._config["num_attention_heads"],
+            num_kv_heads=self._config["num_key_value_heads"],
+            dim=self._config["hidden_size"],
+            head_dim=self._config.get("head_dim", None),
+        )
+
+    if self._adapter_checkpoint:
+        adapter_state_dict = safe_torch_load(self._adapter_checkpoint)
+        converted_state_dict[training.ADAPTER_KEY] = adapter_state_dict
+
+    if self._should_load_recipe_state:
+        recipe_state = safe_torch_load(self._recipe_checkpoint, mmap=False)
+        converted_state_dict.update(recipe_state)
 
     return converted_state_dict
+
+# def load_checkpoint(
+#     ckpt_path: str,
+#     model_name: str,
+# ) -> Dict[str, Any]:
+#     """
+#     Load HF checkpoint from file.
+
+#     The keys and weights from across all checkpoint files are merged into a single state_dict.
+#     We preserve the "state_dict key" <-> "checkpoint file" mapping in weight_map so we can
+#     write the state dict correctly in ``save_checkpoint``.
+
+#     Before returning, the model state dict is converted to a torchtune-compatible format using
+#     the appropriate convert_weights function (depending on ``self._model_type``).
+
+#     Args:
+#         ckpt_path: the path that store the model checkpoint downloaded from huggingface `original`
+#         model_name: the HF model name, such as `meta-llama/Llama-3.2-1B-Instruct`
+#     Returns:
+#         state_dict (Dict[str, Any]): torchtune checkpoint state dict
+
+#     Raises:
+#         ValueError: If the values in the input state_dict are not Tensors
+#     """
+
+#     # merged state_dict contains keys and weights from all the checkpoint files
+#     merged_state_dict: Dict[str, torch.Tensor] = {}
+
+#     # converted_state_dict is the final state_dict passed to the recipe after the
+#     # keys are converted into the torchtune format. This optionally also contains
+#     # the recipe state and adapter weights
+#     converted_state_dict: Dict[str, Dict[str, torch.Tensor]] = {}
+
+#     # _checkpoint_paths are already sorted so simply enumerate to generate the right id
+#     state_dict = safe_torch_load(ckpt_path)
+#     merged_state_dict.update(state_dict)
+
+#     # delete the state_dict to free up memory; TODO check if this del is needed
+#     del state_dict
+#     gc.collect()
+
+#     cfg_dict = MODEL_CONFIG_DICT[model_name]
+#     converted_state_dict = convert_weights.hf_to_tune(
+#         merged_state_dict,
+#         num_heads=cfg_dict["num_attention_heads"],
+#         num_kv_heads=cfg_dict["num_key_value_heads"],
+#         dim=cfg_dict["hidden_size"],
+#         head_dim=cfg_dict.get("head_dim", None),
+#     )
+
+#     return converted_state_dict
 
 
 
